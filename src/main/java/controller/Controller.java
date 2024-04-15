a1816902e50e59d612cc11b1f48bce08470ac746
hahaHoho21 <hahahoho2103@gmail.com>
2024-04-15 16:25:06 +0900

    리치앤고
    리치앤고

---
+++ src/main/java/controller/Controller.java
@@ -0,0 +1,21 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/c")
+public class Controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public Controller() {
+        super();
+    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.getRequestDispatcher("/WEB-INF/views/richgo.jsp").forward(request, response);
+	}
+}
