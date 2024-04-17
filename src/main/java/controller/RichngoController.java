c20a2769f623fc6dfc5beedd17be0e2f8e0d35ac
hahaHoho21 <hahahoho2103@gmail.com>
2024-04-17 15:19:20 +0900

    클래스 이름변경
    클래스 이름변경

--- src/main/java/controller/RichngoController.java
+++ src/main/java/controller/RichngoController.java
@@ -8,10 +8,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/go")
-public class Controller extends HttpServlet {
+public class RichngoController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	public Controller() {
+	public RichngoController() {
         super();
     }
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
